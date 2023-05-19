218b3f4bfc6c7542257c3d42e71987c5b853d465
root <root@ip-172-31-25-143.us-east-2.compute.internal>
2023-05-19 06:29:56 +0000

    first_ec2

--- main.tf
+++ main.tf
@@ -19,7 +19,7 @@ provider "aws" {
 resource "aws_instance" "ubuntu" {
   ami           = "${var.ami_id}"
   instance_type = "${var.instance_type}"
-  key_name = "${var.instance_type}"
+  key_name = "${var.key_name}"
   associate_public_ip_address = true
 
   tags = {
